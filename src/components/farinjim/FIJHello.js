f3c72632530aaa93bc4ebe178d2ef25fc38ef320
Speirsy <jpspeirs@icloud.com>
2023-04-07 13:36:51 +0100

    fixed transparency issue for main image on FIJHello

--- src/components/farinjim/FIJHello.js
+++ src/components/farinjim/FIJHello.js
@@ -7,7 +7,9 @@ const FIJHello = () => {
   return (
     <>
     
+   
       <div className="main-image">
+        
       <header> 
         <div className="title">Far In Jim
         </div> 
@@ -19,7 +21,7 @@ const FIJHello = () => {
 
         My mum says that building a website is a waste of time and no one will visit it because my songs aren't very good. 
         But I don't accept that. <a href='https://www.youtube.com/watch?v=0hBAQuQVxEY'>"Hey There Andy Murray"</a> got thousands of views in 2013 which is like a million in today's money, and some people come back and watch it sometimes. And it was in the papers. So it is a good song. 
-        She says "The Spiral" got, like, no views (which isn't true) but I've had people message me from the spiral saying they went there because of my video. "Should I Move Out Of My Mum's House" is a song I'm really proud of and it doesn't matter if she thinks it's rubbish. 
+        She says <a href='https://www.youtube.com/watch?v=Cxcxt_KRpNk'>"The Spiral"</a> got, like, no views (which isn't true) but I've had people message me from the spiral saying they went there because of my video. "Should I Move Out Of My Mum's House" is a song I'm really proud of and it doesn't matter if she thinks it's rubbish. 
         I got paid actual money to write "Sports Personality" and that's called a commision. My mum borrowed the money though and never paid me back. She says "Dear Santa" is ok but only cause she gets a mention in it. I don't think she's even listened to any of the others. 
         But I don't care what she thinks anymore. I'm going to keep doing it anyway. 
 
