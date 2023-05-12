4d215c09fbe07789563e48efa0fea6ef8b35562b
Speirsy <jpspeirs@icloud.com>
2023-05-12 14:19:21 +0100

    I'm Back

--- src/components/farinjim/FIJHello.js
+++ src/components/farinjim/FIJHello.js
@@ -20,12 +20,12 @@ const FIJHello = () => {
         <p className="flexbox-item flexbox-item-1">
 
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         My mum says that building a website is a waste of time and no one will visit it because my songs aren't very good. 
         But I don't accept that. <a href='https://www.youtube.com/watch?v=0hBAQuQVxEY'>"Hey There Andy Murray"</a> got thousands of views in 2013 which is like a million in today's money, and some people come back and watch it sometimes. And it was in the papers. So it is a good song. 
         She says <a href='https://www.youtube.com/watch?v=Cxcxt_KRpNk'>"The Spiral"</a> got, like, no views (which isn't true) but I've had people message me from the spiral saying they went there because of my video. "Should I Move Out Of My Mum's House" is a song I'm really proud of and it doesn't matter if she thinks it's rubbish. 
         I got paid actual money to write "Sports Personality" and that's called a commision. My mum borrowed the money though and never paid me back. She says "Dear Santa" is ok but only cause she gets a mention in it. I don't think she's even listened to any of the others. 
-        But I don't care what she thinks anymore. I'm going to keep doing it anyway. 
+        But I don't care what she thinks anymore. I'm going to keep doing it anyway.  */}
 
         </p>
 
