4ac414bcb23eebbea6bd2a6f0353c727e6760a26
Speirsy <jpspeirs@icloud.com>
2023-05-12 16:02:44 +0100

    You can filter the videos array based on the artist field to create separate arrays for each artist. Then, you can render the videos in each array in separate unordered lists.

--- src/components/farinjim/FIJVideos.js
+++ src/components/farinjim/FIJVideos.js
@@ -14,24 +14,41 @@ useEffect(() => {
   .then(data => setVideos(data))  //sets our state in the component
 },[]);
 
+const speirsyVideos = videos.filter((video) => video.artist === 'speirsy');
+const fijVideos = videos.filter((video) => video.artist === 'fij');
 
   
 // the virtual dom wants the array to have an index key for the mapping . hence ",index" and key = {index} in the li. 
-  const htmlSongArray = videos.map((songItem, index) => (
+
+
+  const speirsyJSXArray = speirsyVideos.map((video, index) => (
     <li key={index}>
-    <h3>{songItem.name}</h3>
-    <h4>{songItem.year}</h4>
-    <h4>{songItem.artist}</h4>
+      <h3>{video.name}</h3>
+      <h4>{video.year}</h4>
+      <h4>{video.artist}</h4>
     
-  <iframe 
-// this iframe business is a somewhat hardcoded soulution  and needs sorting out. 
-  width="400" height="285" className="video-item "
-  src={songItem.url} 
-  title="YouTube video player" frameBorder="0" 
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>     
-  </iframe>
-    </li>
-     
+      <iframe 
+        width="400" height="285" className="video-item "
+        src={video.url} 
+        title="YouTube video player" frameBorder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>     
+      </iframe>
+    </li>   
+  ));
+
+  const fijJSXArray = fijVideos.map((video, index) => (
+    <li key={index}>
+      <h3>{video.name}</h3>
+      <h4>{video.year}</h4>
+      <h4>{video.artist}</h4>
+    
+      <iframe 
+        width="400" height="285" className="video-item "
+        src={video.url} 
+        title="YouTube video player" frameBorder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>     
+      </iframe>
+    </li>   
   ));
   return (
     <>
@@ -41,7 +58,7 @@ useEffect(() => {
       </header>
 
     <ul>
-      {htmlSongArray}
+      {fijJSXArray}
     </ul>
      <title>
       Speirsy, Not Jim
@@ -49,9 +66,9 @@ useEffect(() => {
      <div>
       Speirsy toons
      </div>
-     {/* <ul>
-      {speirsyVideoArray}
-     </ul> */}
+     <ul>
+      {speirsyJSXArray}
+     </ul>
 
    
   
@@ -60,3 +77,61 @@ useEffect(() => {
 };
 
 export default FIJLook;
+
+// below is a refactoring by Chat GPT
+// The main changes are:
+
+// Extracting the video rendering logic into a separate VideoList component.
+// Using filter to split the videos into separate arrays based on their artist field.
+// Rendering the VideoList component twice, passing in the filtered arrays as props.
+
+// import React, { useState, useEffect } from 'react';
+// import './FIJVideos.css';
+// import { getVideos } from '../../repository';
+
+// const VideoList = ({ videos }) => (
+//   <ul>
+//     {videos.map((video) => (
+//       <li key={video.id}>
+//         <h3>{video.name}</h3>
+//         <h4>{video.year}</h4>
+//         <h4>{video.artist}</h4>
+//         <iframe
+//           width="400"
+//           height="285"
+//           className="video-item"
+//           src={video.url}
+//           title="YouTube video player"
+//           frameBorder="0"
+//           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+//           allowFullScreen
+//         />
+//       </li>
+//     ))}
+//   </ul>
+// );
+
+// const FIJLook = () => {
+//   const [videos, setVideos] = useState([]);
+
+//   useEffect(() => {
+//     getVideos().then((data) => setVideos(data));
+//   }, []);
+
+//   const fijVideos = videos.filter((video) => video.artist === 'fij');
+//   const speirsyVideos = videos.filter((video) => video.artist === 'speirsy');
+
+//   return (
+//     <>
+//       <header>
+//         <h1 className="videos-header">FIJ Videos</h1>
+//       </header>
+//       <VideoList videos={fijVideos} />
+//       <title>Speirsy, Not Jim</title>
+//       <div>Speirsy toons</div>
+//       <VideoList videos={speirsyVideos} />
+//     </>
+//   );
+// };
+
+// export default FIJLook;
