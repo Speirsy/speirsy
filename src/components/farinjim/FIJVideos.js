f6747bc8af8c9540a199084ac9c7ba305437b1f1
Speirsy <jpspeirs@icloud.com>
2023-05-15 14:08:16 +0100

    FIJVideos refacrtored DRY. Attmepts to import components for reuse

--- src/components/farinjim/FIJVideos.js
+++ src/components/farinjim/FIJVideos.js
@@ -1,82 +1,84 @@
 
-import React, {useState, useEffect} from 'react';
-import './FIJVideos.css';
-import { getVideos } from '../../repository';
+// import React, {useState, useEffect} from 'react';
+// import './FIJVideos.css';
+// import { getVideos } from '../../repository';
 
 
 
-const FIJLook = () => {
+// const FIJLook = () => {
 
-const [videos, setVideos] = useState([]);  
+// const [videos, setVideos] = useState([]);  
 
-useEffect(() => {
-  getVideos()
-  .then(data => setVideos(data))  //sets our state in the component
-},[]);
+// useEffect(() => {
+//   getVideos()
+//   .then(data => setVideos(data))  //sets our state in the component
+// },[]);
 
-const speirsyVideos = videos.filter((video) => video.artist === 'speirsy');
-const fijVideos = videos.filter((video) => video.artist === 'fij');
+// const speirsyVideos = videos.filter((video) => video.artist === 'speirsy');
+// const fijVideos = videos.filter((video) => video.artist === 'fij');
 
   
-// the virtual dom wants the array to have an index key for the mapping . hence ",index" and key = {index} in the li. 
+// // the virtual dom wants the array to have an index key for the mapping . hence ",index" and key = {index} in the li. 
 
 
-  const speirsyJSXArray = speirsyVideos.map((video, index) => (
-    <li key={index}>
-      <h3>{video.name}</h3>
-      <h4>{video.year}</h4>
-      <h4>{video.artist}</h4>
+//   const speirsyJSXArray = speirsyVideos.map((video, index) => (
+//     <li key={index}>
+//       <h3>{video.name}</h3>
+//       <h4>{video.year}</h4>
+//       <h4>{video.artist}</h4>
     
-      <iframe 
-        width="400" height="285" className="video-item "
-        src={video.url} 
-        title="YouTube video player" frameBorder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>     
-      </iframe>
-    </li>   
-  ));
+//       <iframe 
+//         width="400" height="285" className="video-item "
+//         src={video.url} 
+//         title="YouTube video player" frameBorder="0" 
+//         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>     
+//       </iframe>
+//     </li>   
+//   ));
 
-  const fijJSXArray = fijVideos.map((video, index) => (
-    <li key={index}>
-      <h3>{video.name}</h3>
-      <h4>{video.year}</h4>
-      <h4>{video.artist}</h4>
+//   const fijJSXArray = fijVideos.map((video, index) => (
+//     <li key={index}>
+//       <h3>{video.name}</h3>
+//       <h4>{video.year}</h4>
+//       <h4>{video.artist}</h4>
     
-      <iframe 
-        width="400" height="285" className="video-item "
-        src={video.url} 
-        title="YouTube video player" frameBorder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>     
-      </iframe>
-    </li>   
-  ));
-  return (
-    <>
+//       <iframe 
+//         width="400" height="285" className="video-item "
+//         src={video.url} 
+//         title="YouTube video player" frameBorder="0" 
+//         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>     
+//       </iframe>
+//     </li>   
+//   ));
+//   return (
+//     <>
    
-      <header>
-        <h1 className="videos-header">FIJ Videos</h1>
-      </header>
+//       <header>
+//         <h1 className="videos-header">FIJ Videos</h1>
+//       </header>
 
-    <ul>
-      {fijJSXArray}
-    </ul>
-     <title>
-      Speirsy, Not Jim
-     </title>
-     <div>
-      Speirsy toons
-     </div>
-     <ul>
-      {speirsyJSXArray}
-     </ul>
+//     <ul>
+//       {fijJSXArray}
+//     </ul>
+//      <title>
+//       Speirsy, Not Jim
+//      </title>
+//      <div>
+//       Speirsy toons
+//      </div>
+//      <ul>
+//       {speirsyJSXArray}
+//      </ul>
 
    
   
-    </>
-  );
-};
+//     </>
+//   );
+// };
 
-export default FIJLook;
+// export default FIJLook;
+
+
 
 // below is a refactoring by Chat GPT
 // The main changes are:
@@ -85,53 +87,56 @@ export default FIJLook;
 // Using filter to split the videos into separate arrays based on their artist field.
 // Rendering the VideoList component twice, passing in the filtered arrays as props.
 
-// import React, { useState, useEffect } from 'react';
-// import './FIJVideos.css';
-// import { getVideos } from '../../repository';
+import React, { useState, useEffect } from 'react';
+import './FIJVideos.css';
+import { getVideos } from '../../repository';
 
-// const VideoList = ({ videos }) => (
-//   <ul>
-//     {videos.map((video) => (
-//       <li key={video.id}>
-//         <h3>{video.name}</h3>
-//         <h4>{video.year}</h4>
-//         <h4>{video.artist}</h4>
-//         <iframe
-//           width="400"
-//           height="285"
-//           className="video-item"
-//           src={video.url}
-//           title="YouTube video player"
-//           frameBorder="0"
-//           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-//           allowFullScreen
-//         />
-//       </li>
-//     ))}
-//   </ul>
-// );
+const VideoList = ({ videos }) => (
+  <ul>
+    {videos.map((video) => (
+      <li key={video.id}>
+        <h3>{video.name}</h3>
+        <h4>{video.year}</h4>
+        <h4>{video.artist}</h4>
+        <iframe
+          width="400"
+          height="285"
+          className="video-item"
+          src={video.url}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        />
+      </li>
+    ))}
+  </ul>
+);
 
-// const FIJLook = () => {
-//   const [videos, setVideos] = useState([]);
+const FIJLook = () => {
+  const [videos, setVideos] = useState([]);
 
-//   useEffect(() => {
-//     getVideos().then((data) => setVideos(data));
-//   }, []);
+  useEffect(() => {
+    getVideos().then((data) => setVideos(data));
+  }, []);
 
-//   const fijVideos = videos.filter((video) => video.artist === 'fij');
-//   const speirsyVideos = videos.filter((video) => video.artist === 'speirsy');
+  const fijVideos = videos.filter((video) => video.artist === 'fij');
+  const speirsyVideos = videos.filter((video) => video.artist === 'speirsy');
 
-//   return (
-//     <>
-//       <header>
-//         <h1 className="videos-header">FIJ Videos</h1>
-//       </header>
-//       <VideoList videos={fijVideos} />
-//       <title>Speirsy, Not Jim</title>
-//       <div>Speirsy toons</div>
-//       <VideoList videos={speirsyVideos} />
-//     </>
-//   );
-// };
+  return (
+    <>
+      <header>
+        <h1 className="videos-header">FIJ Videos</h1>
+      </header>
+      <VideoList videos={fijVideos} />
+      <title>Speirsy, Not Jim</title>
+      <div>Speirsy toons</div>
+      <VideoList videos={speirsyVideos} />
+    </>
+  );
+};
 
-// export default FIJLook;
+
+
+export default FIJLook;
+export {VideoList};
--- src/components/songs5000/5000Hello.js
+++ src/components/songs5000/5000Hello.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { getVideos } from '../../repository';
+import FIJLook from '../farinjim/FIJVideos';
+import { VideoList } from '../farinjim/FIJVideos';
 
 const SongsHello = () => {
+
 
   return (
     <>
     <h1>Songs Hello</h1>
+    <VideoList videos={ speirsyVideos } />
     </>
   );
 };
